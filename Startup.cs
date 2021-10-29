1166b51dad573e9a436c9a1ba8bb7e8733faa6f1
Alexcppio <test21112019test@gmail.com>
2021-10-29 13:33:16 +0500

    project prepared

--- Startup.cs
+++ Startup.cs
@@ -7,27 +7,32 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
 
 namespace WebApp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+        public IConfiguration Configuration { get; set; }
         public void ConfigureServices(IServiceCollection services)
         {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
+            services.AddDbContext<DataContext>(opts =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
+                opts.UseSqlServer(Configuration["ConnectionStrings:ProductConnection"]);
+                opts.EnableSensitiveDataLogging(true);
+            });
+        }
+        public void Configure(IApplicationBuilder app, DataContext context)
+        {
+            app.UseDeveloperExceptionPage();
             app.UseRouting();
-
+            app.UseMiddleware<TestMiddleware>();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
@@ -35,6 +40,7 @@ namespace WebApp
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+            SeedData.SeedDatabase(context);
         }
     }
 }
