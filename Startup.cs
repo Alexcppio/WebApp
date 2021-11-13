ccdbbf0425467397a6ecf4eb93de18a673fe65df
Alexcppio <test21112019test@gmail.com>
2021-11-13 13:13:32 +0500

    test included tag helpers

--- Startup.cs
+++ Startup.cs
@@ -3,8 +3,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
-using Microsoft.AspNetCore.Razor.TagHelpers;
-using WebApp.TagHelpers;
+//using Microsoft.AspNetCore.Razor.TagHelpers;
+//using WebApp.TagHelpers;
 
 namespace WebApp
 {
@@ -25,8 +25,8 @@ namespace WebApp
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddRazorPages().AddRazorRuntimeCompilation();
             services.AddSingleton<CitiesData>();
-            services.AddTransient<ITagHelperComponent, TimeTagHelperComponent>();
-            services.AddTransient<ITagHelperComponent, TableFooterTagHelperComponent>();
+            //services.AddTransient<ITagHelperComponent, TimeTagHelperComponent>();
+            //services.AddTransient<ITagHelperComponent, TableFooterTagHelperComponent>();
         }
         public void Configure(IApplicationBuilder app, DataContext context)
         {
