d73e756ddc32b40c5cda14dafb77bfbd68500a45
Alexcppio <test21112019test@gmail.com>
2021-11-18 15:14:49 +0500

    filters add

---
+++ Filters/ChangePageArgs.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace WebApp.Filters
+{
+    public class ChangePageArgs : Attribute, IPageFilter
+    {
+        public void OnPageHandlerSelected(PageHandlerSelectedContext context)
+        {
+            // Ничего не делать
+        }
+        public void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+        {
+            if(context.HandlerArguments.ContainsKey("message1"))
+            {
+                context.HandlerArguments["message1"] = "New message";
+            }
+        }
+        public void OnPageHandlerExecuted(PageHandlerExecutedContext context)
+        {
+            // Ничего не делать
+        }
+    }
+}
